fb722f663ec229928633fd90a12c8e2b0cd71126
ulugbekovbehruzbekd007 <ulugbekovbehruzbek.d@gmail.com>
2026-03-09 16:18:11 +0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,7 @@
 name = input("What's your name? ")
 print(f"Hello, {name}! It is my first git commit!🎉")
+
+#new code
+surname = input("What's your surname? ")
+age = int(input("Your age: "))
+print(f"Your surname is {age}, your age is {age}")
