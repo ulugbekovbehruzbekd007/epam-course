dcc0683e85b9f00eebd87d0b91527acd76b17982
James <ulugbekovbehruzbek.d@gmail.com>
2026-03-13 06:21:26 +0500

    commit

--- hello.py
+++ hello.py
@@ -4,4 +4,5 @@ print(f"Hello, {name}! It is my first git commit!🎉")
 #new code
 surname = input("What's your surname? ")
 age = int(input("Your age: "))
-print(f"Your surname is {age}, your age is {age}")
+city = input("Your city: ")
+print(f"{name} from {city}, your surname is {surname}, your age is {age}")
