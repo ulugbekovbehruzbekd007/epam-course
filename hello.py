2bef144740a2025d329b32db03df6b38ef7d17af
James <ulugbekovbehruzbek.d@gmail.com>
2026-03-15 14:42:30 +0500

    greeting feature qo'shildi

--- hello.py
+++ hello.py
@@ -1,10 +1,16 @@
 name = input("What's your name? ")
 print(f"Hello, {name}! It is my first git commit!🎉")
-print("-------------------------------")
+
 #new code
 surname = input("What's your surname? ")
 age = int(input("Your age: "))
 city = input("Your city: ")
-print(f"{name} from {city}, your surname is {surname}, your age is {age}")
-print("Exciting")
+if age < 18:
+	print(f"{name} from {city}, your surname is {surname}, your age is {age}, you look young!")
+elif age < 30:
+	 print(f"{name} from {city}, your surname is {surname}, your age is {age}, you look professional!")
+else:
+	 print(f"{name} from {city}, your surname is {surname}, your age is {age}, old school babe")
+
+
 
